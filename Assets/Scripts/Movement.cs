a0bd2994e318bbe7480a8e554f304d27d3f0f638
b4t54ndw1ch <jagoblacksky@gmail.com>
2023-08-16 19:15:58 -0500

    6. basic input binding

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -13,6 +13,34 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
     }
+
+    void ProcessThrust() 
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Pressed SPACE - Thrusting");
+        }
+    }
+    
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            // rotate left
+            Debug.Log("Rotating left");
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            // rotate right
+            Debug.Log("Rotating right");
+        }        
+    }
+
+
+
+
 }
