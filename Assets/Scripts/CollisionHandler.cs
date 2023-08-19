8e199f0aeb3fbbcb9bcecee335b3f4370835dc51
b4t54ndw1ch <jagoblacksky@gmail.com>
2023-08-18 20:24:14 -0500

    15.Respawnusing scene manager

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -20,14 +21,19 @@ public class CollisionHandler : MonoBehaviour
                 break;
 
             default:
-                Debug.Log ("You crashed");
+                ReloadLevel();
                 break;
 
-
-
-
-
         }
+
+        void ReloadLevel()
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; 
+            SceneManager.LoadScene(currentSceneIndex);
+        }
+
+
+
     }
 
 
